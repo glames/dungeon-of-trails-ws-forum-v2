aa341ccab7c1aee184ece3b8852076055b27db57
James Pham <jamespham.study@gmail.com>
2023-12-05 17:32:07 +0700

    Update app.tsx

--- dungeon-of-trails-forum-client/src/app/app.tsx
+++ dungeon-of-trails-forum-client/src/app/app.tsx
@@ -38,7 +38,7 @@ import ProtectedRoute from './UI/components/ProtectedRoute';
 import withAuthentication from './UI/components/WithAuthenticationComponent';
 
 const httpLink = createHttpLink({
-  uri: `https://localhost:${7017}/graphql`,
+  uri: `http://dungeonoftrialsapiservice-dev.eba-de5bcp3p.ap-southeast-1.elasticbeanstalk.com/graphql`,
   fetch,
 });
 
