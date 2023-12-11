86f649cbafc21fd43db70847a1624befbdb52521
James Pham <jamespham.study@gmail.com>
2023-12-11 13:40:56 +0700

    Update build

---
+++ dungeon-of-trails-forum-client/build/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as UserJpg } from './User.jpg';
+export { default as UserPng } from './user/user.png';
+export { default as Notification1 } from './notification/1.jpg';
+export { default as Notification2 } from './notification/2.jpg';
