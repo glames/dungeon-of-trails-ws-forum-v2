28e2bb06cb2f6725bd2bd1db106776ebd05924b6
James Pham <jamespham.study@gmail.com>
2023-12-17 20:27:37 +0700

    update

--- dungeon-of-trails-forum-client/webpack.config.js
+++ dungeon-of-trails-forum-client/webpack.config.js
@@ -7,17 +7,10 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
   config.plugins.push(new Dotenv());
   config.output = {
     filename: '[name].bundle.js',
-    path: path.resolve(
-      '/home/runner/work/dungeon-of-trails-ws-forum-v2/dungeon-of-trails-ws-forum-v2/dungeon-of-trails-forum-client/build'
-    ), // Use an absolute path
+    path: path.resolve(__dirname, 'build'), // Use an absolute path
   };
   // Disable CSS minification
-  console.log(
-    'HIHIHI: ' +
-      path.resolve(
-        '/home/runner/work/dungeon-of-trails-ws-forum-v2/dungeon-of-trails-ws-forum-v2/dungeon-of-trails-forum-client/build'
-      )
-  );
+  console.log('HIHIHI: ' + path.resolve(__dirname, 'build'));
   config.optimization.minimize = false;
   return config;
 });
